097c8ff55d3cfc16fd5b2502e934608aaeaa1c6a
jorgelfer <jorgeferram17@gmail.com>
2025-05-28 12:43:49 -0500

    first commit dont know where

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page of our application.</p>
+      <p>Here you can find information about our app and its features.</p>
+    </div>
+  );
+}
