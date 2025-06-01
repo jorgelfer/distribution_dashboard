9931a20848d2f132b4e52e62fccb881b852daa6d
jorgelfer <jorgeferram17@gmail.com>
2025-06-01 11:28:58 -0500

    case selection working

--- UI/login.js
+++ UI/login.js
@@ -1,49 +1,48 @@
 import React from "react";
 import styles from "./login.module.css";
 import CaseFormSubmit from "@/components/case/case-form-submit";
-import getDSS from "@/lib/actions";
+import { getDSS } from "@/lib/actions";
 
-// ({ values, onEnteredValues, onSubmitted })
-const networks = [
-  { value: "3Bus", label: "3Bus", inFile1: "3Bus-DY.dss" },
-  { value: "4Bus", label: "4Bus", inFile1: "4Bus-DY-reg.dss" },
-  { value: "123Bus", label: "123Bus", inFile1: "IEEE123Master.dss" },
-  { value: "secondary", label: "secondary", inFile1: "Master.dss" },
-  { value: "8500Node", label: "8500Node", inFile1: "Master-unbal.dss" },
-  { value: "8500Node_120", label: "8500Node_120", inFile1: "Master-unbal.dss" },
-  { value: "8500Node_1ph", label: "8500Node_1ph", inFile1: "Master.dss" },
-  { value: "8500Node_441", label: "8500Node_441", inFile1: "Master.dss" },
-  { value: "8500Node_510", label: "8500Node_510", inFile1: "Master-unbal.dss" },
-  {
-    value: "center_tapped",
-    label: "center_tapped",
-    inFile1: "center_tap_xfmr.dss",
-  },
-];
+import { infile1_map } from "@/models/case";
 
-export default function Login() {
+export default function Login({ values, onEnteredValues }) {
+  console.log(values);
   return (
     <>
       <header className={styles.header}>
         <h1>Case definition</h1>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={getDSS}>
           <div className={styles.row}>
             <p>
               <label htmlFor="dropdown">Network Model</label>
-              <select id="network-model">
-                <option value="13Bus">13 Bus</option>
-                {networks.map((network) => (
-                  <option key={network.value} value={network.value}>
-                    {network.label}
+              <select
+                id="network-model"
+                name="network-model"
+                onChange={(event) =>
+                  onEnteredValues("networkModel", event.target.value)
+                }
+                value={values.networkModel}
+              >
+                {Object.keys(infile1_map).map((network) => (
+                  <option key={network} value={network}>
+                    {network}
                   </option>
                 ))}
               </select>
             </p>
             <p>
               <label htmlFor="text">InFile1</label>
-              <input id="infile1" type="text" name="infile1" />
+              <input
+                id="infile1"
+                type="text"
+                name="infile1"
+                onChange={(event) =>
+                  onEnteredValues("inFile1", event.target.value)
+                }
+                value={values.inFile1}
+              />
             </p>
           </div>
           <p className={styles.actions}>
--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <>
       <main>
-        <Login />
+        <Login values={enteredCase} onEnteredValues={handleInputChange} />
       </main>
     </>
   );
